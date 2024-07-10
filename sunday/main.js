8fd92e50d4b26967b154a3b07fac7b0775c3fe10
Marina <marina@192.168.1.4>
2024-07-10 20:36:47 +0300

    add Daniels project

---
+++ sunday/main.js
@@ -0,0 +1,91 @@
+function getPrice(prise, ads, tax, profit){
+    var result = prise + ads
+    var result1 = result + result1*tax
+    var finalResult = result1 + profit
+
+
+
+    return finalResult;
+}
+console.log((getPrice)(80,20,.1,50));
+
+
+
+
+
+
+
+
+
+
+
+
+//var userRole = "ahmed";
+//if(userRole === 'admin')
+//{
+   // console.log('you can add , edit ,delet')
+//}
+//else if(userRole ==='sales')
+//{
+    //console.log('you can add only')
+//}
+//else if(userRole ==='markter')
+//{
+   // console.log('you can edit only')
+//}
+//else
+//{
+    //console.log('rawa7')
+//}
+//switch(userRole)
+//{
+   // case "admin":
+   //     console.timeLog('you can add only');
+   // break;
+   // case "markter":
+     //   console.log('you can edit only');
+    //break;
+    
+   // default:
+     //   console.log('rawa7')
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var userName = "ahmed aly" //string
+var userChar = "a" //string
+var userCode = "18733773" //string
+var prodectCode = "56574478" //string
+var userAge = 29; //number
+var prodectPrice = 74829842.888; //number
+var tax = 0.9888 //number
+var onSale = true; //boolean
+var uttlittles = null;
+  
+
+console.log( typeof(userName));
+console.log(typeof(userAge));
+console.log(typeof(onSale));
+
+
+
+
+
+
+
+
+//window.alter('hello route');
+//console.log('hello console');
+//document.getElementById('demo').innerHTML="";
